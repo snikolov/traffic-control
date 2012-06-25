7ad16ea6251f3c96f810be665e44dbe1f04733e6
Stanislav Nikolov <snikolov@twitter.com>
2012-06-25 04:47:15 -0400

    Got the system to be controllable. Now it gives a gain matrix

--- src/test_traffic_lqr.m
+++ src/test_traffic_lqr.m
@@ -9,13 +9,13 @@ k2=1;
 n_cars=5;
 A=zeros(n_cars*2);
 C=diag(-1*ones(n_cars,1))+diag(ones(n_cars-1,1),-1);
-%C(1,n_cars)=1;
+C(1,n_cars)=1;
 A(1:n_cars,n_cars+1:2*n_cars)=C;
 A(n_cars+1:2*n_cars,n_cars+1:2*n_cars)=k2*C;
 A(n_cars+1:2*n_cars,1:n_cars)=k1*diag(ones(n_cars,1));
 
 % Indices of actuated cars.
-active=2;
+active=[2,3];
 n_active=numel(active);
 A(active+n_cars,:)=0;
 B=eye(2*n_cars);
