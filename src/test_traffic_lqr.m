8a1460022464f908039a2b1f8d51fc48deeb5e61
Stanislav Nikolov <snikolov@twitter.com>
2012-06-26 04:40:03 -0400

    Optionize collision and backward movement avoidance.

--- src/test_traffic_lqr.m
+++ src/test_traffic_lqr.m
@@ -6,16 +6,16 @@ global A B K n_cars n_active x1 vd dd
 % Gains for unactuated cars.
 k1=2;
 k2=2;
-n_cars=55;
+n_cars=30;
 A=zeros(n_cars*2);
 C=diag(-1*ones(n_cars,1))+diag(ones(n_cars-1,1),-1);
-%C(1,n_cars)=1;
+C(1,n_cars)=1;
 A(1:n_cars,n_cars+1:2*n_cars)=C;
 A(n_cars+1:2*n_cars,n_cars+1:2*n_cars)=k2*C;
 A(n_cars+1:2*n_cars,1:n_cars)=k1*diag(ones(n_cars,1));
 
 % Indices of actuated cars.
-active=[n_cars]
+active=[1:3:n_cars]
 n_active=numel(active);
 A(active+n_cars,:)=0;
 B=eye(2*n_cars);
@@ -35,7 +35,7 @@ Co=ctrb(A,B);
 rank(Co)
 [K,S,e]=lqr(A,B,Q,R)
 
-x=[1*rand(n_cars,1);1;zeros(n_cars-1,1)];
+x=[rand(n_cars,1);1;zeros(n_cars-1,1)];
 run(x);
 
 function run(x)
@@ -44,17 +44,23 @@ global x1 vd n_cars dd xmax
 xmax=max(x(1:n_cars));
 dt=0.01;
 figure
+NO_COLLIDE=0;
+NO_BACKWARD=0;
 for tidx=1:100000
   u=control(x);
   xdot=dynamics(x,u);
-  % Make sure cars don't collide.
-  collisions=x(1:n_cars)<-dd/10;
-  x(collisions)=0;
-  xdot(collisions)=0;
-  % Make sure cars don't move backward.
-  backward=x(n_cars+1:2*n_cars)<0;
-  x(n_cars+backward)=0;
-  xdot(backward)=0;
+  if ~NO_COLLIDE
+    % Make sure cars don't collide.
+    collisions=x(1:n_cars)<-dd/10;
+    x(collisions)=0;
+    xdot(collisions)=0;
+  end
+  if ~NO_BACKWARD
+    % Make sure cars don't move backward.
+    backward=x(n_cars+1:2*n_cars)<0;
+    x(n_cars+backward)=0;
+    xdot(backward)=0;
+  end
   x=x+dt*xdot;
   x1=x1+dt*(vd+xdot(n_cars+1));
   if ~mod(tidx,100)
