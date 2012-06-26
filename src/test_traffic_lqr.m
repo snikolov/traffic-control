2f73f889db3297e07f46174dea513c8d06edfebd
Stanislav Nikolov <snikolov@twitter.com>
2012-06-26 00:06:23 -0400

    Better plotting. Now with actual motion.

--- src/test_traffic_lqr.m
+++ src/test_traffic_lqr.m
@@ -1,25 +1,32 @@
 % LQR-based traffic smoothing.
 
 function test_traffic_lqr
-global A B K n_cars n_active
+global A B K n_cars n_active x1 vd dd
 
 % Gains for unactuated cars.
-k1=1;
-k2=1;
-n_cars=10;
+k1=2;
+k2=2;
+n_cars=30;
 A=zeros(n_cars*2);
 C=diag(-1*ones(n_cars,1))+diag(ones(n_cars-1,1),-1);
-C(1,n_cars)=1;
+%C(1,n_cars)=1;
 A(1:n_cars,n_cars+1:2*n_cars)=C;
 A(n_cars+1:2*n_cars,n_cars+1:2*n_cars)=k2*C;
 A(n_cars+1:2*n_cars,1:n_cars)=k1*diag(ones(n_cars,1));
 
 % Indices of actuated cars.
-active=[1,2,3];
+active=1:3:n_cars;
 n_active=numel(active);
 A(active+n_cars,:)=0;
 B=eye(2*n_cars);
 B=B(:,n_cars+active);
+
+% Keep track of the position of the first car
+x1=0;
+% Desired velocity
+vd=2;
+% Desired intercar distance
+dd=2;
 
 % LQR
 Q=eye(2*n_cars);
@@ -28,18 +35,23 @@ Co=ctrb(A,B);
 rank(Co)
 [K,S,e]=lqr(A,B,Q,R)
 
-x=[1*ones(n_cars,1);1;zeros(n_cars-1,1)];
+x=[1*rand(n_cars,1);1;zeros(n_cars-1,1)];
 run(x);
 
 function run(x)
-dt=0.1;
+global x1 vd n_cars
+dt=0.01;
 figure
-for tidx=1:1000
-  t=dt*tidx;
+for tidx=1:10000
   u=control(x);
   xdot=dynamics(x,u);
+  collisions=x(1:n_cars)<=0;
+  xdot(collisions)=0;
   x=x+dt*xdot;
-  plot_cars(x);
+  x1=x1+dt*(vd+xdot(n_cars+1));
+  if ~mod(tidx,50)
+    plot_cars(x);
+  end
 end
 
 function xdot=dynamics(x,u)
@@ -51,12 +63,21 @@ global K
 u=-K*x;
 
 function plot_cars(x)
-global n_cars
-pos=cumsum(x(1:n_cars));
-subplot(211)
-scatter(pos,zeros(size(pos)),'ks','SizeData',5)
-xlim([-2*n_cars n_cars*2])
-subplot(212)
-stem(x(1:n_cars))
+global n_cars dd vd x1
+pos=x1-(cumsum(x(1:n_cars)+dd));
+subplot(411)
+scatter(pos,zeros(size(pos)),'ks','SizeData',10)
+xlim([-3*n_cars n_cars*3])
+ylim([-2,2])
+title('Cars')
+subplot(412)
+stem(x(1:n_cars)+dd)
+title('Spacings')
+subplot(413)
+stem(pos)
+title('Positions')
+subplot(414)
+stem(vd+x(n_cars+1:2*n_cars))
+title('Velocities')
 drawnow;
 pause(0.01)
