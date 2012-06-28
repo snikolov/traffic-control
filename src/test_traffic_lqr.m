4316f91aafe48ce4a814a40ab8152c0e43ab975f
Stanislav Nikolov <snikolov@twitter.com>
2012-06-28 02:42:01 -0400

    Plotting everything relative to last car rather than first car. Still doesn't fix the problem of perceived backward motion. Changed A matrix so that all vbar_{i-1}-vbar_i becomes -vbar_i. This way the desired velocity is obviously achieved.

--- src/test_traffic_lqr.m
+++ src/test_traffic_lqr.m
@@ -1,19 +1,19 @@
 % LQR-based traffic smoothing.
 
 function test_traffic_lqr
-global A B K n_cars n_active x1 vd dd topology radius dmin
+global A B K n_cars n_active xlast vd dd topology radius dmin
 
 close all
 
 % Gains for unactuated cars.
 k1=2;
 k2=2;
-n_cars=55;
+n_cars=80;
 A=zeros(n_cars*2);
-C=diag(-1*ones(n_cars,1))+diag(ones(n_cars-1,1),-1);
+C=diag(-1*ones(n_cars,1));%+diag(ones(n_cars-1,1),-1);
 topology='loop';
 if strcmpi(topology,'loop')
-  C(1,n_cars)=1;
+  %C(1,n_cars)=1;
 end
 A(1:n_cars,n_cars+1:2*n_cars)=C;
 A(n_cars+1:2*n_cars,n_cars+1:2*n_cars)=k2*C;
@@ -25,7 +25,7 @@ A(n_cars+1:2*n_cars,1:n_cars)=k1*diag(ones(n_cars,1));
 %end
 
 % Indices of actuated cars.
-active=[10,20,30,40]
+active=[]
 n_active=numel(active);
 if n_active>0
   A(active+n_cars,:)=0;
@@ -42,18 +42,18 @@ else
   K=0;
 end
 
-% Keep track of the position of the first car
-x1=0;
+% Keep track of the position of the last car
+xlast=0;
 % Desired velocity
 vd=2;
 % Desired intercar distance
 dd=2;
 % Minimum intercar distance
-dmin=0.25;
+dmin=2;
 
-x=[0.1*rand(n_cars,1);vd;zeros(n_cars-1,1)];
+x=[1*rand(n_cars,1);vd;zeros(n_cars-1,1)];
 if strcmpi(topology,'loop')
-  radius=dd*n_cars*1.1/(2*pi);
+  radius=dd*n_cars*1.3/(2*pi);
   x(1)=radius*2*pi-sum(x(2:n_cars)+dd)-dd;
   if x(1)<-dd
     error('Radius is too small');
@@ -62,10 +62,10 @@ end
 run(x);
 
 function run(x)
-global x1 vd n_cars dd dmin xmax
+global xlast vd n_cars dd dmin xmax
 % Boundary for plotting
 xmax=max(x(1:n_cars));
-dt=0.1;
+dt=0.01;
 figure
 NO_COLLIDE=1;
 NO_BACKWARD=0;
@@ -76,17 +76,19 @@ for tidx=1:100000
     % Make sure cars don't collide.
     collisions=x(1:n_cars)<dmin-dd;
     x(collisions)=dmin-dd;
+    x(n_cars+collisions)=0;
     xdot(collisions)=0;
   end
   if NO_BACKWARD
     % Make sure cars don't move backward.
     backward=x(n_cars+1:2*n_cars)<0;
-    x(n_cars+backward)=0;
-    xdot(backward)=0;
+    x(n_cars+backward)=-vd;
+    xdot(backward)=-vd;
+    xdot(n_cars+backward)=0;
   end
   x=x+dt*xdot;
-  x1=x1+dt*(vd+xdot(n_cars+1));
-  if ~mod(tidx,1)
+  xlast=xlast+dt*(vd+xdot(2*n_cars));
+  if ~mod(tidx,4)
     plot_cars(x,xdot);
     % disp(sum(collisions)/n_cars)
   end
@@ -101,12 +103,13 @@ global K
 u=-K*x;
 
 function plot_cars(x,xdot)
-global n_cars dd vd x1 xmax topology radius
+global n_cars dd vd xlast xmax topology radius
 
-pos=[x1;x1-(cumsum(x(2:n_cars)+dd))];
+pos=flipud([xlast;xlast+cumsum(flipud(x(2:n_cars))+dd)]);
 
 subplot(4,2,[1,3,5,7])
 if strcmpi(topology,'line')
+  x1=xlast+sum(x(2:n_cars));
   if x1>xmax
     xmax=x1+5*dd;
   end
@@ -116,7 +119,7 @@ if strcmpi(topology,'line')
   title('Cars')
 elseif strcmpi(topology,'loop')
   spacings=x(1:n_cars)+dd;
-  thetas=[x1;x1-cumsum(spacings(2:end))]/radius;
+  thetas=pos/radius;
   theta_goals=thetas-dd/radius;
   scatter(radius*cos(thetas),radius*sin(thetas),linspace(10,80,n_cars),linspace(1,32,n_cars),'filled');
   % hold on
